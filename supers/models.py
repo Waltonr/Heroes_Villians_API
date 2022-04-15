43f7bbb2d719bfc031dc68ddd41d401c311482c1
Waltonr <waltonr18126@gmail.com>
2022-04-15 15:39:20 -0400

    Created Supers Model for Supers App and Super_Types Model for Super_Types App + Registered Supers, Super_Types, and Rest Framework in Project Settings Installed App Section

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,13 @@
+from tkinter import CASCADE
 from django.db import models
+from super_types.models import Super_Types
 
 # Create your models here.
+
+class Supers(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    super_types = models.ForeignKey(Super_Types, on_delete=models.CASCADE )
