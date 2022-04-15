e5a5389e4607dc006649b7210f6cebfec460af63
Waltonr <waltonr18126@gmail.com>
2022-04-15 16:45:45 -0400

    Created Serializer Class and File + Updated Supers Model

---
+++ super_types/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Super_Types
+
+class Super_TypesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super_Types
+        fields = ['id', 'type']
---
+++ supers/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Supers
+
+class SupersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supers
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
+
+    super_type_id = serializers.IntegerField(write_only=True)
