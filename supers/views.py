dadefeb6022263a70f336f01f0f09ee372e16600
Waltonr <waltonr18126@gmail.com>
2022-04-18 12:32:13 -0400

    Created Post Functionality for Supers

--- supers/views.py
+++ supers/views.py
@@ -1,9 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Supers
 from .serializers import SupersSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    return Response()
+    if request.method == 'GET':
+        supers = Supers.objects.all()
+        serializer = SupersSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
