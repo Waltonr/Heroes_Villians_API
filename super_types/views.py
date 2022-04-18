cb26c566a6c0d64e304cb87227cecb9f652dd95a
Waltonr <waltonr18126@gmail.com>
2022-04-18 11:43:41 -0400

    Registered Super Types Model with Admin Site + Updated Project URls + Created First View Function + Created Supers App and Super Types App URLs File

--- super_types/views.py
+++ super_types/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Super_Types
+from .serializers import Super_TypesSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def super_types_list(request):
+    return Response('ok')
+
+
+
+
+
 
