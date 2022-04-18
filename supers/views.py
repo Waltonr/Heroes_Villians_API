3f3b55c2f3c1ec3dd610a261ce9a01848db4c6eb
Waltonr <waltonr18126@gmail.com>
2022-04-18 14:59:46 -0400

    Created New View Function for Getting Supers by Id + Added New URLs Path for super_detail View Function

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,3 +17,10 @@ def supers_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def supers_details(request, pk):
+    supers = get_object_or_404(Supers, pk=pk)
+    serializer = SupersSerializer(supers)
+    return Response(serializer.data)
